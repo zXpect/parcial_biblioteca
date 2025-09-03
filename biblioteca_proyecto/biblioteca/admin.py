0af8e3b7869268c7503e477172f65aeee7d95003
Breider Lopez <yessidlopez2018@gmail.com>
2025-09-03 15:22:29 -0500

    admin.py: Configuracion del admin

--- biblioteca_proyecto/biblioteca/admin.py
+++ biblioteca_proyecto/biblioteca/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+# biblioteca/admin.py
+
+from django.contrib import admin
+from .models import Autor, Editorial, Libro, Miembro, Prestamo
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido']
+    search_fields = ['nombre', 'apellido']
+    list_filter = ['nombre']
+
+@admin.register(Editorial)
+class EditorialAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'direccion', 'telefono']
+    search_fields = ['nombre']
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor', 'editorial', 'año_publicacion']
+    list_filter = ['autor', 'editorial', 'año_publicacion']
+    search_fields = ['titulo', 'isbn']
+
+@admin.register(Miembro)
+class MiembroAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'apellido', 'email', 'fecha_membresia']
+    search_fields = ['nombre', 'apellido', 'email']
+    list_filter = ['fecha_membresia']
+
+@admin.register(Prestamo)
+class PrestamoAdmin(admin.ModelAdmin):
+    list_display = ['libro', 'miembro', 'fecha_prestamo', 'fecha_devolucion', 'esta_devuelto']
+    list_filter = ['fecha_prestamo', 'fecha_devolucion']
+    search_fields = ['libro__titulo', 'miembro__nombre']
